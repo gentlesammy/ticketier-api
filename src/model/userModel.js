f3566a41ff44242f9cf6cc50897ab4942a5c32f7
Gentlesammy <samodunlade84@gmail.com>
2021-04-18 08:47:47 +0100

    user Schema done

---
+++ src/model/userModel.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 50,
+  },
+  company: {
+    type: String,
+    required: true,
+    maxlength: 50,
+  },
+  address: {
+    type: String,
+    maxlength: 100,
+  },
+  phone: {
+    type: Number,
+    maxlength: 13,
+  },
+  email: {
+    type: String,
+    required: true,
+    maxlength: 50,
+  },
+  password: {
+    type: String,
+    minlength: 8,
+    required: true,
+    maxlength: 50,
+  },
+});
+
+module.exports = model("User", userSchema);
